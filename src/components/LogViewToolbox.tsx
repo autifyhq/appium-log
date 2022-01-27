ed39317402973473915be0680b2546ed16aed4ff
FujiHaruka <fizzyjohn1427@gmail.com>
2022-01-27 15:25:05 +0900

    search input

--- src/components/LogViewToolbox.tsx
+++ src/components/LogViewToolbox.tsx
@@ -1,9 +1,32 @@
 import { React } from "../deps.ts";
 
+const useSearchText = () => {
+  const [text, setText] = React.useState("");
+  return {
+    text,
+    setText,
+  };
+};
+
+const SearchInput = () => {
+  return (
+    <p className="control has-icons-left">
+      <input
+        className="input"
+        type="text"
+        placeholder="Search"
+      />
+      <span className="icon is-small is-left">
+        <i className="fas fa-search"></i>
+      </span>
+    </p>
+  );
+};
+
 export const LogViewToolbox: React.VFC = () => {
   return (
     <section className="section">
-      toolbox
+      <SearchInput />
     </section>
   );
 };
