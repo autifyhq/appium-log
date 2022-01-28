87fde479641e094008a46b8c20dac8483332877c
FujiHaruka <fizzyjohn1427@gmail.com>
2022-01-28 21:11:15 +0900

    toolbox

--- src/components/LogViewToolbox.tsx
+++ src/components/LogViewToolbox.tsx
@@ -8,18 +8,21 @@ const SearchInput: React.VFC<
   onChange,
 }) => {
   return (
-    <p className="control has-icons-left">
-      <input
-        className="input"
-        type="text"
-        placeholder="Search"
-        value={value}
-        onChange={(ev) => onChange(ev.target.value)}
-      />
-      <span className="icon is-small is-left">
-        <i className="fas fa-search"></i>
-      </span>
-    </p>
+    <div className="mr-2">
+      <label className="is-size-7">search</label>
+      <div className="control has-icons-left">
+        <input
+          className="input is-small"
+          type="text"
+          placeholder="Search"
+          value={value}
+          onChange={(ev) => onChange(ev.target.value)}
+        />
+        <span className="icon is-small is-left">
+          <i className="fas fa-search"></i>
+        </span>
+      </div>
+    </div>
   );
 };
 
@@ -30,15 +33,18 @@ const SearchContextLineInput: React.VFC<
   onChange,
 }) => {
   return (
-    <p className="control">
-      <input
-        className="input"
-        type="number"
-        value={value}
-        onChange={(ev) => onChange(Number(ev.target.value))}
-        min={0}
-      />
-    </p>
+    <div className="mr-2 max-width-3em">
+      <label className="is-size-7">lines</label>
+      <div className="control">
+        <input
+          className="input is-small"
+          type="number"
+          value={value}
+          onChange={(ev) => onChange(Number(ev.target.value))}
+          min={0}
+        />
+      </div>
+    </div>
   );
 };
 
@@ -49,11 +55,15 @@ export const LogViewToolbox: React.VFC<{ store: Store }> = ({ store }) => {
   } = store;
   return (
     <section className="log-view-toolbox px-4 py-2">
-      <SearchInput value={search.text} onChange={search.setText} />
-      <SearchContextLineInput
-        value={contextLines.count}
-        onChange={contextLines.setCount}
-      />
+      <div className="level">
+        <div className="level-left">
+          <SearchInput value={search.text} onChange={search.setText} />
+          <SearchContextLineInput
+            value={contextLines.count}
+            onChange={contextLines.setCount}
+          />
+        </div>
+      </div>
     </section>
   );
 };
